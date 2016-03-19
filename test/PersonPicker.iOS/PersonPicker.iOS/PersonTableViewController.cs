9a73d02332d6e996f50e676a338968179ed415aa
Jason Farrell <jfarrell@westmonroepartners.com>
2016-03-19 16:14:04 -0500

    First Page
    Added first page with data

---
+++ test/PersonPicker.iOS/PersonPicker.iOS/PersonTableViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Foundation;
+using Saitama.Apple.Controllers;
+using PersonPicker.iOS.Services;
+using UIKit;
+
+namespace PersonPicker.iOS
+{
+    [Register("PersonTableViewController")]
+    public class PersonTableViewController : SaitamaTableViewController
+    {
+        public IPersonService PersonService { get; set; }
+
+        public PersonTableViewController(IntPtr ptr) : base(ptr)
+        {
+        }
+
+        public override nint RowsInSection(UIKit.UITableView tableView, nint section)
+        {
+            return PersonService.GetPeople().Count;
+        }
+
+        public override UIKit.UITableViewCell GetCell(UIKit.UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = new UITableViewCell(UITableViewCellStyle.Default, "DefaultCell");
+            var dataItem = PersonService.GetPeople().ElementAt(indexPath.Row);
+
+            cell.TextLabel.Text = dataItem.FullName;
+            return cell;
+        }
+    }
+}
+
